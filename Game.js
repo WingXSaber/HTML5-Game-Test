d32577e2668258a2564c24d97aac8af5fedc10b3
WingXSaber <77001066+WingXSaber@users.noreply.github.com>
2021-01-05 20:33:57 +0800

    Update Game.js

--- Game.js
+++ Game.js
@@ -44,7 +44,7 @@ function saveFile(blob, fileName) {
 function intialize(){  
 	
 	//////////////////////
-	console.log("TESTING AHHHHHHHHHHH");	
+	console.log("TESTING TESTING");	
 	var thisIsAString = "AHHHHHHH"
 	const blob = new Blob([thisIsAString], {type : 'application/json'});
 	saveFile(blob, 'test.txt');
